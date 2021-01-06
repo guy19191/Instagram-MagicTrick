abfd4f8323df60867b0353240b1099f35aaf8296
guy19191 <63824070+guy19191@users.noreply.github.com>
2021-01-06 22:07:31 +0200

    Update config.js

--- tests/config.js
+++ tests/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     USER: 'Username', //Enter your instegram user name
-    PASS: 'Pssword' //Enter your instegram password
+    PASS: 'Password' //Enter your instegram password
 }
